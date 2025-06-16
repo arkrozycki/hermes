5cbc3379e076a74e80a29f975f740a4c686bb645
ark <ark@arks-MacBook-Air.local>
2025-06-16 17:14:28 -0500

    i dnno

--- frontend/renderer/lib/config.ts
+++ frontend/renderer/lib/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
+  apiUrl: process.env.NEXT_PUBLIC_API_URL,
   isAuthEnabled: process.env.NEXT_PUBLIC_AUTH_ENABLED === 'true',
   isDevelopment: process.env.NODE_ENV === 'development',
 }
